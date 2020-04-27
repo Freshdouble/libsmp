97bec106f4aa2201587a6c8cc26eff714deabf9e
Peter Kremsner <peterkremsner@gmx.at>
2020-04-27 23:28:54 +0200

    Updated projectfile

---
+++ C#/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using libsmp;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smp = new SMP();
+        }
+    }
+}
